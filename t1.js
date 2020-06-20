158b181005faf053fcc5cc6b2e0403ea7d927bb3
Ivan <yoda.van@yandex.ru>
2020-06-20 21:01:16 +0300

    Есть господь на белом свете. Оно починилось, как только я заменил === на ==. А, но ещё добавил функцию, чтобы спрашивать количество фильмов.

--- t1.js
+++ t1.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let numberOfFilms;
+let numberOfFilms = 0;
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -13,7 +13,7 @@ let personalMovieDB = {
 function start() {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
-    while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)) {
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) === true) {
         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
     }
 }
@@ -27,16 +27,29 @@ function countTest(obj) {
         console.log("Найди себе уже бабу");
 }
 
-// for (let i = 0; i < 2; i++) {
-//     let film = prompt("Какой фильм вы недавно посмотрели?");
-//     let rate = prompt("На сколько оцените его?");
-//     if (film != null && rate != null && film.length !== 0 && film.length <= 50) {
-//         personalMovieDB.movies[film] = rate;
-//     } else {
-//         console.log('Вы дибил');
-//         i--;
-//     }
+function showMyFilms(hidden) {
+    if  (!hidden)
+        console.log(personalMovieDB);
+}
+
+// function writeYourGenres () {
+//     for (let i = 1; i )
 // }
+
+function filmsWatched() {
+    for (let i = 0; i < 2; i++) {
+        let film = prompt("Какой фильм вы недавно посмотрели?");
+        let rate = prompt("На сколько оцените его?");
+        if (film != null && rate != null && film.length !== 0 && film.length <= 50) {
+            personalMovieDB.movies[film] = rate;
+        } else {
+            console.log('Вы дибил');
+            i--;
+        }
+    }
+}
 
 start();
 countTest(personalMovieDB);
+filmsWatched();
+showMyFilms(personalMovieDB.private);
